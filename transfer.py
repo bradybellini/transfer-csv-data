d9fe97512d3685d245ec13afe27a74b956741b0c
Brady <47389606+bradybellini@users.noreply.github.com>
2019-05-07 12:55:57 -0700

    Update transfer.py

--- transfer.py
+++ transfer.py
@@ -1,13 +1,17 @@
 import csv
 
-with open('testing.csv', 'r') as csv_file:
-	csv_reader = csv.reader(csv_file, dialect='excel')
-	with open('bad_score.csv', 'w') as bad_score:
-		csv_writer = csv.writer(bad_score)
+def open_files():
+	with open('testing.csv', 'r') as csv_file, open('bad_score.csv', 'w', newline='') as bad_score:
+		csv_reader = csv.reader(csv_file, dialect='excel')
+		csv_writer = csv.writer(bad_score, dialect='excel')
 		next(csv_reader)
-		csv_writer.writerow(bad_score.row[0])
-		for row in csv_reader:
-			if not '1' in row[30]:
-				csv_writer.writerow(row)
+		find_1(csv_reader, csv_writer)
 			
-f.close()
+def find_1(reader, writer):			
+	for row in reader:
+		if any(field.strip() for field in row):
+			if '1' in row[30]:
+				writer.writerow(row)
+
+if __name__ == '__main__':
+	open_files()
